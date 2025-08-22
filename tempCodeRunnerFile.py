31d9aafb8cef385ce212e1bf637d6788e00f9589
Vishesh1164 <srivastavavishesh59@gmail.com>
2025-08-23 00:04:27 +0530

    AI_article_generator using langchain and flask

--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-query = input("Enter your query: ")
-while(query!='exit'):
-    output = chain.invoke({"query": query})
-    print(output.content)
-
-    memory.save_context({"query": query}, {"output": output.content})
-    query = input("Enter your query: ")
